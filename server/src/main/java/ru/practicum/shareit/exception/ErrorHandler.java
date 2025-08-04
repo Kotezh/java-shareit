64e3907b072d87b9cc82157a528358e32e695e8b
kotezhda <nadinekoteg@gmail.com>
2025-08-04 09:04:10 +0300

    fix: for code review

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,44 +1,35 @@
 package ru.practicum.shareit.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
-        return new ErrorResponse("Error", e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationException(final ValidationException e) {
-        return new ErrorResponse("Error", e.getMessage());
+        log.error("Искомый объект не найден {}", e.getMessage());
+        return new ErrorResponse("Искомый объект не найден", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleInternalServerErrorException(final InternalServerErrorException e) {
-        return new ErrorResponse("Error", e.getMessage());
+        log.error("Ошибка сервера {}", e.getMessage());
+        return new ErrorResponse("Ошибка сервера", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflictException(final ConflictException e) {
-        return new ErrorResponse("Error", e.getMessage());
+        log.error("Произошел конфликт {}", e.getMessage());
+        return new ErrorResponse("Произошел конфликт", e.getMessage());
     }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleBadRequestException(final BadRequestException e) {
-        return new ErrorResponse("Error", e.getMessage());
-    }
-
 }
 
 
